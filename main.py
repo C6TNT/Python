e66cd04622f6c221da764f8d916865b588f47f4c
C6TNT <140625546+C6TNT@users.noreply.github.com>
2024-03-02 21:36:02 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,56 @@
+#print('Albert Einstein once said,"A person who never made a mistake never tried anything new."')
+#famous_person = "Albert Einstein"
+#message = '"A person who never made a mistake never tried anything new."'
+#print(f'{famous_person} once said,{message}')
+#name = " Eler \n\t Ser "
+#print(name.strip())
+#x = 6
+#message = "I Like"
+#print(f"{message} {x}")
+# 向大家问好
+#print("Hello Python people!")
+#import this
+#bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+#message = f"My first bicycle was a {bicycles[0].title()}."
+#print(message)
+#motorcycles = ['honda', 'yamaha', 'suzuki']
+#print(motorcycles)
+
+#motorcycles.append('ducati')
+#print(motorcycles)
+#motorcycles = []
+#motorcycles.append('honda')
+#motorcycles.append('yamaha')
+#motorcycles.append('suzuki')
+#motorcycles.insert(1, 'ducati')
+#print(motorcycles)
+#del motorcycles[1]
+#print(motorcycles)
+
+#motorcycles = ['honda', 'yamaha', 'suzuki']
+#print(motorcycles)
+
+#too_ex = 'honda'
+#motorcycles.remove(too_ex)
+#print(motorcycles)
+#print(f"A {too_ex.title()} is too expensive for me")
+
+#cars = ['bmw', 'audi', 'toyota', 'subaru']
+#cars.sort(reverse=True)
+#print(cars)
+#print("Here is the original list:")
+#print(cars)
+
+#print("\nHere is the sorted list:")
+#print(sorted(cars, reverse=True))
+#print(cars)
+
+#magicians = ['alice', 'david', 'carolina']
+#for magician in magicians:
+#    print(magician)
+#    print(f"I Love {magician.title()}.\n")
+#print("Thank You!")
+
+for value in range(1, 6):
+    print(value)
+
